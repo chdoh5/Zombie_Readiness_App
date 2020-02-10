183b164e524edb547e5e2b64eaa6089c171addb8
github username <github email address>
2020-02-10 17:56:56 -0600

    seed file and update to class Weapon

--- db/migrate/20200210220129_create_weapons.rb
+++ db/migrate/20200210220129_create_weapons.rb
@@ -1,6 +1,7 @@
 class CreateWeapons < ActiveRecord::Migration[6.0]
   def change
     create_table :weapons do |t|
+      t.string :name
       t.string :user_id
       t.integer :readiness_value
     end
