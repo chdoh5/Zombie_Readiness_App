280b7bc3119b2989e99b182ec9483bf10d5f920e
Andrea <andreabarr87@gmail.com>
2020-02-12 16:56:24 -0600

    spiffed up application/hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
     def hello 
         redirect_to controller: 'sessions', action: 'new' unless session[:user_id]
+        @supplies = Supply.all
+        @owned_items = OwnedItem.all
       end
 
 
