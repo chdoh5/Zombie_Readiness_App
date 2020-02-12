ab9ae7d71cd10f9979d4eb96e8cd5f80ecb37933
github username <github email address>
2020-02-12 13:54:00 -0600

    fixed creation and seed

--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -7,7 +7,7 @@ class WeaponsController < ApplicationController
 
     def update
         @weapon = Weapon.find(params[:id])
-        @user = User.find_by(name: session[:name])
+        @user = User.find(session[:user_id])
         @weapon.update(user_id: @user.id)
         redirect_to user_path(@user)
     end 
