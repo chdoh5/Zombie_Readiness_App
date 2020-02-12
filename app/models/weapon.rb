f781548ab0ac8307d5a775b3c5d18dc3734931be
Andrea <andreabarr87@gmail.com>
2020-02-12 10:56:43 -0600

    added validations to all models

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -3,5 +3,11 @@ class Weapon < ApplicationRecord
 	has_one :group, through: :user
 	has_many :owned_items, through: :user
 	has_many :supplies, through: :owned_items
+
+	validates :name, presence: true
+	validates :name, uniqueness: true
+	validates :user_id, numericality: { only_integer: true }, allow_nil: true
+	validates :readiness_value, presence: true
+	validates :readiness_value, numericality: { only_integer: true }
 	
 end
